b83e5145549d7966d0fef19d1bddc45547b4bd12
Emilie <emiliempark@gmail.com>
2020-03-06 01:26:58 +1300

    Add Axios and test api

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../logo.svg';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 function App() {
+  
+  useEffect(() => {
+    axios.get('https://virtserver.swaggerhub.com/narrative-software/sci-fi-movie-api/1.0.0/movies').then(res => {console.log(res)});
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
